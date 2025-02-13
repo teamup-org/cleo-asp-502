5c94ae3bd7e5b7dc0f5fb9c2a8bbc94aa98ea623
clairey660 <78447875+clairey660@users.noreply.github.com>
2025-02-12 22:38:50 -0600

    testing

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module CleoCourseScheduler
     config.autoload_lib(ignore: %w[assets tasks])
     # Configuration for the application, engines, and railties goes here.
     #
+    puts "Looking for: #{File.expand_path('config/initializers/assets.rb', __dir__)}"
+
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
