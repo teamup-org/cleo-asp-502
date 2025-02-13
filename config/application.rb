672a1219231bd7e5cc459bdfb69a6e24ac1e2890
clairey660 <78447875+clairey660@users.noreply.github.com>
2025-02-12 20:41:23 -0600

    updated bundler

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,6 @@ module CleoCourseScheduler
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
-    require_relative 'initializers/assets' if File.exist?(File.join(__dir__, 'initializers/assets.rb'))
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
