3686c6944168f2db507f185c0985ed8fe22fe1e2
clairey660 <78447875+clairey660@users.noreply.github.com>
2025-03-05 16:54:28 -0600

    included degree planner import and export with tests

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,11 @@ Rails.application.routes.draw do
 
   # Admin dashboard
   namespace :admin do
-    resources :prerequisites do 
+    resources :prerequisites do
       member do
         get :confirm_destroy
       end
-    end    
+    end
     resources :student_courses, param: :student_id do
       get ':course_id', action: :show, on: :member
       get ':course_id/edit', action: :edit, on: :member, as: 'edit'
@@ -89,6 +89,7 @@ Rails.application.routes.draw do
 
     resource :degree_planner, only: [:show], controller: 'degree_planner' do
       patch :update_plan, on: :member
+      patch 'add_course', to: 'degree_planners#add_course', on: :member
       post :generate_custom_plan, on: :member
       delete 'remove_course', on: :member
       get :download_plan, on: :member
